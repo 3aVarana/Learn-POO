c2881e1375afee74317f5d5a5b0e62cf6a3161c6
Andres Arana <3av.arana@gmail.com>
2023-03-13 22:23:44 -0400

    Move implementation from Book.h to Book.cpp

---
+++ src/Book.cpp
@@ -0,0 +1,71 @@
+#include "Book.h"
+
+Book::Book(string title)
+{
+    this->title = title;
+    this->genre = BookGenre::unknown;
+    this->authors = vector<Author *>();
+}
+
+Book::Book(string title, BookGenre genre)
+{
+    this->title = title;
+    this->genre = genre;
+    this->authors = vector<Author *>();
+}
+
+Book::Book(string title, BookGenre genre, vector<Author *> authors)
+{
+    this->title = title;
+    this->genre = genre;
+    this->authors = authors;
+}
+
+string Book::getTitle()
+{
+    return title;
+}
+
+string Book::getEditorial()
+{
+    return editorial;
+}
+
+string Book::getGenre()
+{
+    switch (genre)
+    {
+    case fiction:
+        return "Fiction";
+        break;
+
+    case historic:
+        return "Historic";
+        break;
+
+    case educative:
+        return "Educative";
+        break;
+
+    default:
+        return "Unknown";
+    }
+}
+
+void Book::addAuthor(Author *author)
+{
+    authors.push_back(author);
+}
+
+void Book::showInfo()
+{
+    cout << "Book's title: " << title << endl;
+    cout << "Book's editorial: " << editorial << endl;
+    cout << "Book's genre: " << getGenre() << endl;
+    cout << "Athors:\n";
+    for (Author *author : authors)
+    {
+        cout << author->getName() << endl;
+    }
+    cout << endl;
+}
--- src/Book.h
+++ src/Book.h
@@ -23,72 +23,13 @@ private:
     vector<Author *> authors;
 
 public:
-    Book(string title)
-    {
-        this->title = title;
-        this->genre = BookGenre::unknown;
-        this->authors = vector<Author *>();
-    }
-    Book(string title, BookGenre genre)
-    {
-        this->title = title;
-        this->genre = genre;
-        this->authors = vector<Author *>();
-    }
+    Book(string title);
+    Book(string title, BookGenre genre);
+    Book(string title, BookGenre genre, vector<Author *> authors);
 
-    Book(string title, BookGenre genre, vector<Author *> authors)
-    {
-        this->title = title;
-        this->genre = genre;
-        this->authors = authors;
-    }
-
-    string getTitle()
-    {
-        return title;
-    }
-
-    string getEditorial()
-    {
-        return editorial;
-    }
-
-    string getGenre()
-    {
-        switch (genre)
-        {
-        case fiction:
-            return "Fiction";
-            break;
-
-        case historic:
-            return "Historic";
-            break;
-
-        case educative:
-            return "Educative";
-            break;
-
-        default:
-            return "Unknown";
-        }
-    }
-
-    void addAuthor(Author *author)
-    {
-        authors.push_back(author);
-    }
-
-    void showInfo()
-    {
-        cout << "Book's title: " << title << endl;
-        cout << "Book's editorial: " << editorial << endl;
-        cout << "Book's genre: " << getGenre() << endl;
-        cout << "Athors:\n";
-        for (Author *author : authors)
-        {
-            cout << author->getName() << endl;
-        }
-        cout << endl;
-    }
+    string getTitle();
+    string getEditorial();
+    string getGenre();
+    void addAuthor(Author *author);
+    void showInfo();
 };
