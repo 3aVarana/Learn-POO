4f1a86d37db775637ed61ea7b97a2c2023a2adee
Andres Arana <3av.arana@gmail.com>
2023-03-13 21:38:49 -0400

    Solve merge conflicts

--- src/Application.h
+++ src/Application.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Author.h"
+#include "Book.h"
 #include <iostream>
 using namespace std;
 
@@ -8,6 +10,21 @@ class Application
 public:
     void run()
     {
-        cout << "It's working" << endl;
+        vector<Author *> authors;
+        authors.push_back(new Author("Edgar Allan Poe"));
+        authors.push_back(new Author("Gabriel Garcia Marquez"));
+        authors.push_back(new Author("James Patterson"));
+        authors.push_back(new Author("David Baldacci"));
+        authors.push_back(new Author("J D Robb"));
+
+        for (Author *a : authors)
+        {
+            a->showInfo();
+        }
+
+        for (Author *a : authors)
+        {
+            delete a;
+        }
     }
 };
