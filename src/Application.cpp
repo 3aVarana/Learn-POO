853644193da09c95431ccb63e49054a3c381f65b
Andres Arana <3av.arana@gmail.com>
2023-03-13 22:28:40 -0400

    Refactor code

---
+++ src/Application.cpp
@@ -0,0 +1,21 @@
+#include "Application.h"
+
+void Application::run()
+{
+    vector<Author *> authors;
+    authors.push_back(new Author("Edgar Allan Poe"));
+    authors.push_back(new Author("Gabriel Garcia Marquez"));
+    authors.push_back(new Author("James Patterson"));
+    authors.push_back(new Author("David Baldacci"));
+    authors.push_back(new Author("J D Robb"));
+
+    for (Author *a : authors)
+    {
+        a->showInfo();
+    }
+
+    for (Author *a : authors)
+    {
+        delete a;
+    }
+}
--- src/Application.h
+++ src/Application.h
@@ -8,23 +8,5 @@ using namespace std;
 class Application
 {
 public:
-    void run()
-    {
-        vector<Author *> authors;
-        authors.push_back(new Author("Edgar Allan Poe"));
-        authors.push_back(new Author("Gabriel Garcia Marquez"));
-        authors.push_back(new Author("James Patterson"));
-        authors.push_back(new Author("David Baldacci"));
-        authors.push_back(new Author("J D Robb"));
-
-        for (Author *a : authors)
-        {
-            a->showInfo();
-        }
-
-        for (Author *a : authors)
-        {
-            delete a;
-        }
-    }
+    void run();
 };
